bd2996948b25ca03f34f69c2bf24d09bf6e321c4
js <504446405@qq.com>
2019-07-21 14:19:37 +0800

    页面左部分完成

--- components/post/recommend.vue
+++ components/post/recommend.vue
@@ -2,7 +2,13 @@
   <div class="city-recommend">
     <div class="city-recommend-top">
       <ul class="city-recommend-top-father">
-        <li v-for="(v,i) in cityList" :key="i" @mouseenter="fn(i)">
+        <li
+          v-for="(v,i) in cityList"
+          :key="i"
+          @mouseenter="immigrations(i)"
+          @mouseleave="shiftOut"
+          :class="{frame:curr}"
+        >
           <span>{{v}}</span>
           <span>></span>
         </li>
@@ -14,12 +20,24 @@
         v-show="index===num"
       >
         <li v-for="(v,i) in value" :key="i">
-          <span>{{i}}{{v.name}}</span>
+          <span>
+            <i>{{i}}</i>
+            {{v.name}}
+          </span>
           <span>{{v.list}}</span>
         </li>
       </ul>
     </div>
-    <div class="city-recommend-bottom"></div>
+    <div class="city-recommend-bottom">
+      <div class="city-recommend-bottom-title">推荐城市</div>
+      <ul>
+        <li>
+          <nuxt-link to="#">
+            <img src alt />
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -102,13 +120,19 @@ export default {
           }
         ]
       ],
-      num: 0
+      num: null,
+      curr: false
     };
   },
   methods: {
-    fn(i) {
+    // 鼠标移出显示
+    immigrations(i) {
       this.num = i;
       console.log(123);
+    },
+    // 鼠标移出隐藏
+    shiftOut() {
+      this.num = null;
     }
   }
 };
@@ -119,7 +143,12 @@ export default {
   height: 300px;
   .city-recommend-top {
     position: relative;
+
     .city-recommend-top-father {
+      border-right: 1px solid #ddd;
+      &:hover {
+        border: none;
+      }
       li {
         display: flex;
         padding: 10px 20px;
@@ -127,13 +156,14 @@ export default {
         font-size: 14px;
         border: 1px solid #ddd;
         border-bottom: none;
+        border-right: none;
         span:last-child {
           color: #ddd;
           font-family: cursive;
           font-size: 16px;
         }
         &:last-child {
-          border: 1px solid #ddd;
+          border-bottom: 1px solid #ddd;
         }
         &:hover {
           color: orange;
@@ -145,16 +175,40 @@ export default {
       position: absolute;
       top: 0;
       left: 260px;
+      background-color: #fff;
+      font-size: 16px;
       border: 1px solid #ddd;
       border-left: none;
-      width: 350px;
-      // padding: 20px 0;
+      width: 360px;
+      padding: 10px 0;
+      border-left: 1px solid #ddd;
+      border-right: 1px solid #ddd;
       li {
-        padding: 10px 30px;
-        border-left: 1px solid #ddd;
-        border-right: 1px solid #ddd;
+        padding: 10px 20px;
+        span:first-child {
+          font-size: 16px;
+          color: orange;
+          margin-right: 10px;
+          i {
+            font-size: 18px;
+            padding-right: 10px;
+          }
+        }
       }
     }
+  }
+}
+.city-recommend-bottom {
+  margin-top: 10px;
+  .city-recommend-bottom-title {
+    padding: 14px 0;
+    border-bottom: 1px solid #ddd;
+  }
+  li {
+    width: 260px;
+    height: 170px;
+    margin-top: 10px;
+    background-color: rgba(69, 66, 228, 0.2);
   }
 }
 </style>
